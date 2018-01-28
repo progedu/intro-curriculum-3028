4368d6d021f2371fc9f1aa4633e01d49a1ab2c82
H.Kawanami <h.kawa@cap.ocn.ne.jp>
2018-01-28 06:39:46 +0000

    半角スペースが+で表示されるのを修正

--- test.js
+++ test.js
@@ -15,7 +15,21 @@ const html = jade.renderFile('./views/posts.jade', {
   user: 'guest1'
 });
 
+// 半角スペースが正しく表示されることのテスト
+const html_space = jade.renderFile('./views/posts.jade', {
+  posts: [{
+    id: 1,
+    content: 'space test',
+    postedBy: 'guest1',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }],
+  user: 'guest1'
+});
+
 // スクリプトタグがエスケープされて含まれていることをチェック
 assert(html.indexOf('&lt;script&gt;alert(\'test\');&lt;/script&gt;') > 0);
+// 半角スペースが表示されていることをチェック
+assert(html_space.indexOf(' ') > 0);
 console.log('テストが正常に完了しました');
 
