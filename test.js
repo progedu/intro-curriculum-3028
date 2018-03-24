975e932fce4cb604090a6a859763876a8806440c
daisuke iwahashi <iwahashi.daisuke@gmail.com>
2018-03-24 11:11:45 +0000

    +

--- test.js
+++ test.js
@@ -15,7 +15,21 @@ const html = jade.renderFile('./views/posts.jade', {
   user: 'guest1'
 });
 
+//
+const html2 = jade.renderFile('./views/posts.jade', {
+  posts: [{
+    id: 2,
+    content: '+',
+    postedBy: 'guest1',
+    trackingCookie: 1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }],
+  user: 'guest1'
+});
+
 // スクリプトタグがエスケープされて含まれていることをチェック
 assert(html.indexOf('&lt;script&gt;alert(\'test\');&lt;/script&gt;') > 0);
+assert(html2.indexOf(' ') > 0);
 console.log('テストが正常に完了しました');
 
