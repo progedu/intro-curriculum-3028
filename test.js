b2399ca216e209c377fd428d2fa6fef92f8f9998
Your Name <you@example.com>
2020-02-14 23:44:58 +0900

    +を半角スペースに変換

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const pug = require('pug');
 const assert = require('assert');
 
 // pug のテンプレートにおける XSS 脆弱性のテスト
-const html = pug.renderFile('./views/posts.pug', {
+const html1 = pug.renderFile('./views/posts.pug', {
   posts: [{
     id: 1,
     content: '<script>alert(\'test\');</script>',
@@ -16,6 +16,23 @@ const html = pug.renderFile('./views/posts.pug', {
 });
 
 // スクリプトタグがエスケープされて含まれていることをチェック
-assert(html.includes('&lt;script&gt;alert(\'test\');&lt;/script&gt;'));
+assert(html1.includes('&lt;script&gt;alert(\'test\');&lt;/script&gt;'));
+
+// '+' が ' 'に変換されているかテスト
+let content = '1+2+3';
+content = content.replace(/\+/g, ' ');
+const html2 = pug.renderFile('./views/posts.pug', {
+  posts: [{
+    id: 1,
+    content: content,
+    postedBy: 'guest1',
+    trackingCookie: 1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }],
+  user: 'guest1'
+});
+assert(html2.includes('1 2 3'));
+
 console.log('テストが正常に完了しました');
 
