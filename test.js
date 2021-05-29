3c045ad13ccb52ca032caacc31b0aa0157a6cca6
Kengo Yagishita <yagishitakengo1002@gmail.com>
2021-05-29 22:58:54 +0900

    半角スペースを表示できるよう修正

--- test.js
+++ test.js
@@ -3,10 +3,18 @@ const pug = require('pug');
 const assert = require('assert');
 
 // pug のテンプレートにおける XSS 脆弱性のテスト
+// 半角スペースが正しく表示されているかのテストを追加
 const html = pug.renderFile('./views/posts.pug', {
   posts: [{
     id: 1,
     content: '<script>alert(\'test\');</script>',
+    postedBy: 'guest1',
+    trackingCookie: 1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },{
+    id: 2,
+    content: 'space space',
     postedBy: 'guest1',
     trackingCookie: 1,
     createdAt: new Date(),
@@ -17,5 +25,7 @@ const html = pug.renderFile('./views/posts.pug', {
 
 // スクリプトタグがエスケープされて含まれていることをチェック
 assert(html.includes('&lt;script&gt;alert(\'test\');&lt;/script&gt;'));
+// 半角スペースが正しく表示されていることをチェック
+assert(html.includes('space space'));
 console.log('テストが正常に完了しました');
 
